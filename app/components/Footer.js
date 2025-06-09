720f3004b1fcb67665839cf97a7f278f49c6594b
aaryan <166105497+aaryankalra@users.noreply.github.com>
2025-06-09 22:35:56 +0530

    Changed modal
    Changed modal for better UI
    Added basic footer

--- app/components/Footer.js
+++ app/components/Footer.js
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from "react";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <>
+      <div className="bg-base-100 text-gray-600 flex flex-col items-center justify-center pt-18 pb-6">
+        <h1>Created by</h1>
+        <h1>Aaryan</h1>
+      </div>
+    </>
+  );
+};
 
-export default Footer
+export default Footer;
--- app/components/Home/ActivityCard.js
+++ app/components/Home/ActivityCard.js
@@ -18,10 +18,37 @@ const ActivityCard = ({ item }) => {
               Read More
             </button>
             <dialog id="my_modal_1" className="modal">
-              <div className="modal-box">
-                <h3 className="font-bold text-lg">{item.title}!</h3>
-                <p className="py-4">{item.description}</p>
-                <p className="text-gray-500">{item.date}</p>
+              <div className="modal-box flex flex-col pt-0 pb-5">
+                <div className="flex">
+                  <figure>
+                    <img
+                      src={item.imageUrl}
+                      alt="Event Image"
+                      className="h-52 rounded-lg"
+                    />
+                  </figure>
+                  <div className="card-body">
+                    <h2 className="card-title">{item.title}</h2>
+                    <p>{item.description}</p>
+                    <div>
+                      <p className="font-bold">Location:</p>
+                      <div className="flex-col items-center gap-2">
+                        <p>{item.location}</p>
+                        <p className="text-gray-700">{item.date}</p>
+                      </div>
+                    </div>
+                    <div>
+                      <p className="font-bold mb-1">Created by:</p>
+                      <div className="flex items-center gap-2">
+                        <img
+                          src={item.userProfileUrl}
+                          className="h-6 rounded-full"
+                        />
+                        <p>{item.userFullname}</p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
                 <div className="modal-action">
                   <form method="dialog">
                     {/* if there is a button in form, it will close the modal */}
